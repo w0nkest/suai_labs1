261e9b041e8f20a789ded036f8e60dc8b7197e9d
w0nkest <nmdvoret@gmail.com>
2023-11-14 23:01:45 +0300

    lab 3 dop

--- main.py
+++ main.py
@@ -1,3 +1,35 @@
 import laba4
 
-print(laba4.maximum(*[float(input('Введите число: ')) for i in range(4)]))
+nums = []
+
+for i in range(4):
+    while 1:
+        try:
+            n = float(input('Введите число: '))
+        except:
+            n = 10**10
+        if n != 10**10:
+            break
+        print('Это не число')
+    nums.append(n)
+
+print(laba4.maximum(*nums))
+
+# доп
+
+newnums = []
+
+while 1:
+    nums = input('Введите числа, элементы не являющиеся числами будут удалены: ').split()
+    for i in nums:
+        try:
+            i = float(i)
+        except:
+            continue
+        newnums.append(i)
+    if len(newnums) == 0:
+        print('Вы не ввели ни одного числа, повторите попытку')
+    else:
+        break
+
+print(laba4.maximum(*newnums))
